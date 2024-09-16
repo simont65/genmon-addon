2d17ad90c3bcf734015e652fd63d81d6359b5349
Simon Therrien <therriens65@gmail.com>
2024-09-15 21:46:07 -0400

    Update app.py set portal tcp port to 5000

--- CaptivePortal/app.py
+++ CaptivePortal/app.py
@@ -122,4 +122,4 @@ def ShutdownHotspot(ssid, wifi_key):
     subprocess.run(['nmcli', 'conn', 'delete', 'id', 'Hotspot'])
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 80)
+    app.run(host = '0.0.0.0', port = 5000)
